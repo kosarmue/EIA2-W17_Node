ba80d90989dd4c2fa6527f51555301ac2b753e1b
kosarmue <32566225+kosarmue@users.noreply.github.com>
2018-01-24 19:18:33 +0100

    Update ServerTest.ts

--- ServerTest.ts
+++ ServerTest.ts
@@ -47,7 +47,7 @@ namespace ServerTest {
         let query: AssocStringString = Url.parse(_request.url, true).query;
         // ?
         for (let key in query)
-            _response.write(key + ": " + query[key]);
+            _response.write(key + ": " + query[key] + "<br");
         
         // Antwort abschließen und abschicken
         _response.end();
